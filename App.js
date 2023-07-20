15851e48f59127d840212778d38d0959ce172e39
Miguel Cid Y <miguelcidyevenes@gmail.com>
2023-07-20 13:15:17 -0400

    Configuracion de un CRUD para iniciar desde EXPO CLI y RACT NATIVE

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hola Mundo desde mi primer React native APP
+        
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
