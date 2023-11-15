cc7af59f243129702f90fe75c0c5c196822394fc
Miguel Cid Y <miguelcidyevenes@gmail.com>
2023-11-15 08:21:49 -0300

    Instalacion de dependencias React Navigtor y Navigator Stack, creacion de screens con archivos .js, actualizacion de algunas dependencias utilizadas con expo.

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+
+//este Stack va a contener toda nuestra navegacion
+
+const Stack = createStackNavigator()
+
+//para tener mas orden en el codigo, creo una funcion MyStack para crear vistas o screens
+
+function MyStack() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen />
+    </Stack.Navigator>
+
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hola Mundo desde mi primer React native APP
-        
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#41B21E',
     alignItems: 'center',
     justifyContent: 'center',
   },
